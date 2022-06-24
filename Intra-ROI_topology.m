e7e4932284de9dd07f9c371d798f638f9d53b489
AlvaroNovillo <alvanovi@ucm.es>
2022-06-24 12:21:57 +0200

    Topological study of each ROI

--- Intra-ROI_topology.m
+++ Intra-ROI_topology.m
@@ -5,7 +5,7 @@ band = ["alpha"];
 dum = sprintf('../plv_nodes/sexy_cn_312s_plv_1202nodes_%s.mat',band);
 files=dir(dum);
 filename=horzcat(files.folder,'\',files.name);
-load(filename) %DESCARGAR ARCHIVO BETA
+load(filename); %DESCARGAR ARCHIVO BETA
 load("..\fcinfo.mat") %Map nodes 2 ROIs
 %Filter betweeen boys and Girls
 boys = sample.neuro_vals(:,2) == 1; %Boys logical array
@@ -20,7 +20,7 @@ ROIs = fcinfo.rois_in;
 boy_nodes = load_nodes(boys, fcmatrix); %Boys nodes matrix
 girl_nodes = load_nodes(girls, fcmatrix); %Girls nodes matrix
 
-names = {'ROIs_names', 'Clustering', 'C_error','Shortest_Path_Length', 'Dist_error'};
+names = {'ROIs_names', 'Clustering', 'C_error','Shortest_Path_Length', 'Dist_error','Full_C','Full_dist'};
 c = cell(length(names),1);
 results_boys = cell2struct(c,names);
 results_girls = cell2struct(c,names);
@@ -39,6 +39,8 @@ for i = 1:length(ROIs)
     results_boys.ROIs_names(i) = ROI_names(i);
     results_boys.Clustering(i) = mean(C_boys);
     results_boys.Shortest_Path_Length(i) = mean(dist_boys);
+    results_boys.Full_C(:,i) = C_boys;
+    results_boys.Full_dist(:,i) = dist_boys;
     
     %Error
     results_boys.C_error(i) = std(C_boys)./sqrt(length(C_boys));
@@ -58,6 +60,8 @@ for i = 1:length(ROIs)
     results_girls.ROIs_names(i) = ROI_names(i);
     results_girls.Clustering(i) = mean(C_girls);
     results_girls.Shortest_Path_Length(i) = mean(dist_girls);
+    results_girls.Full_C(:,i) = C_girls;
+    results_girls.Full_dist(:,i) = dist_girls;
     
     %Error
     results_girls.C_error(i) = std(C_girls)./sqrt(length(C_girls));
@@ -76,6 +80,15 @@ girls_diag = load_connectivity(girls, fcmatrix);
 results_boys.Connect = boys_diag.Connect'; results_boys.ConnErr = boys_diag.Error';
 results_girls.Connect = girls_diag.Connect'; results_girls.ConnErr = girls_diag.Error';
 %%
+%eTIV and age/sex classification 
+[age_boys,eTIV_boys,study_boys] = neuro_sex_load(boys,sample);
+[age_girls, eTIV_girls,study_girls] = neuro_sex_load(girls,sample);
+
+
+%Add to the result cell
+results_boys.age_boys = age_boys'; results_boys.eTIV_boys = eTIV_boys'; results_boys.study_boys = study_boys';
+results_girls.age_girls = age_girls'; results_girls.eTIV_girls = eTIV_girls'; results_girls.study_girls = study_girls';
+%%
 %Save the results 
 
 save(sprintf('Intra_ROI_results/%s/boys.mat',band),"results_boys")
@@ -90,6 +103,8 @@ load(sprintf('Intra_ROI_results/%s/boys.mat',band))
 load(sprintf('Intra_ROI_results/%s/girls.mat',band))
 %%
 paint_topo(results_boys.Clustering, results_boys.Shortest_Path_Length,results_girls.Clustering,results_girls.Shortest_Path_Length,results_boys.ROIs_names,results_boys.C_error,results_boys.Dist_error,results_girls.C_error,results_girls.Dist_error,results_boys.Connect, results_girls.Connect)
+%%
+select_paint(3,results_boys.ROIs_names{3},results_boys.Full_C,results_boys.Full_dist,results_girls.Full_C,results_girls.Full_dist,results_boys.age_boys,results_boys.eTIV_boys,results_girls.age_girls,results_girls.eTIV_girls,results_boys.study_boys,results_girls.study_girls)
 %%
 function [age_sex,eTIV_sex,study_sex] = neuro_sex_load(sex,sample)
     age_sex = sample.neuro_vals(find(sex),1);
@@ -225,4 +240,85 @@ function paint_topo(C_boys,dist_boys,C_girls,dist_girls,ROI_tag,C_b_err,dist_b_e
     legend('Location',"best")
     title('Shortest path length')
     hold off;
+end
+
+function select_paint(ROI,ROI_name,C_boys,dist_boys,C_girls,dist_girls,age_boys,eTIV_boys,age_girls,eTIV_girls,study_boys,study_girls)
+
+    %C 
+    C_box = [double(C_boys(:,ROI))',double(C_girls(:,ROI))'];
+    g = [ones(size(C_boys(:,ROI)')), 2*ones(size(C_girls(:,ROI)'))]; %Boys and girls class.
+    binEdges = 1:3;
+    bins = {'Man', 'Woman'};
+    groupSex = discretize(g,binEdges,'categorical',bins);
+    
+    
+    %dist
+    dist_box = [dist_boys(:,ROI)',dist_girls(:,ROI)'];
+    g = [ones(size(dist_boys(:,ROI)')), 2*ones(size(dist_girls(:,ROI)'))]; %Boys and girls class.
+    binEdges = 1:3;
+    bins = {'Man', 'Woman'};
+    groupSex = discretize(g,binEdges,'categorical',bins);
+    
+    %C per age
+    figure();
+    ages = [age_boys,age_girls];
+    binEdges = 50:10:90;
+    bins = {'50s','60s','70s','80s'};
+    groupAge = discretize(ages,binEdges,'categorical',bins);
+    boxchart(groupAge,C_box,'GroupByColor',groupSex)
+    title('Clustering vs age')
+    legend('Location',"best")
+    ylabel(sprintf('%s Clustering',ROI_name))
+    xlabel('age')
+    
+    %dist per age
+    figure();
+    boxchart(groupAge,dist_box,'GroupByColor',groupSex)
+    title('Shortest path length vs age')
+    legend('Location',"best")
+    ylabel(sprintf('%s Shortest Path Length',ROI_name))
+    xlabel('age')
+    
+     %C per study_year
+
+    figure();
+    study_period = [study_boys,study_girls];
+    study_period(isnan(study_period)) = 0;
+    binEdges = 0:5:35;
+    bins = {'-5','5','10' ,'15','20','25','+30'};
+    groupStudy = discretize(study_period,binEdges,'categorical',bins);
+    boxchart(groupStudy,C_box,'GroupByColor',groupSex)
+    title('Clustering vs Study period')
+    legend('Location',"best")
+    ylabel(sprintf('%s Clustering',ROI_name))
+    xlabel('Study period')
+    
+    %dist per study_year
+    figure();
+    groupStudy = discretize(study_period,binEdges,'categorical',bins);
+    boxchart(groupStudy,dist_box,'GroupByColor',groupSex)
+    title('Shortest path length vs Study period')
+    legend('Location',"best")
+    ylabel(sprintf('%s Shortest Path Length',ROI_name))
+    xlabel('Study period')
+    
+    %Years of study and age vs C
+    figure();
+    binEdges = 0:10:30;
+    bins = {'0','10','20'};
+    groupStudy = discretize(study_period,binEdges,'categorical',bins);
+    boxchart(groupStudy,C_box,'GroupByColor',groupAge)
+    title('Clustering vs Study period per age')
+    legend('Location',"best")
+    ylabel(sprintf('%s Clustering',ROI_name))
+    xlabel('Study period')
+    
+    %Years of study and age vs dist
+    figure();
+    boxchart(groupStudy,dist_box,'GroupByColor',groupAge)
+    title('Shortest Path Length vs Study period per age')
+    legend('Location',"best")
+    ylabel(sprintf('%s Shortest Path Length',ROI_name))
+    xlabel('Study period')
+
 end
